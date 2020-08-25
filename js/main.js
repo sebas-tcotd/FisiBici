a18d53b56487cb5cb3762e06041c62bf4fde8778
Sebastian Vargas <vargaspizango1@gmail.com>
2020-08-24 22:37:36 -0500

    Agregado landing y shop inicial

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// let gridBicis = document.querySelector(".contenedor.grid-bicis");
+// let biciItem = document.createElement("div");
+// let att = document.createAttribute("class");
+// att.value = "bici-item";
+// biciItem.setAttributeNode(att);
+
+//    debugger;
+//   biciItem.innerHTML = `
+//         <div class="imagen-bici">
+//           <!-- <img src="img/bici${1}.jpg" alt=""> -->
+//         </div>
+//         <p class="nombre-bici">
+//           H. Davidson i8-3300
+//         </p>
+//         <span class="precio">S/ 00.00</span>
+//     `;
+//     biciItem.style.backgroundImage = `url(../img/bici${1}.jpg)`;
+//   gridBicis.append(biciItem);
