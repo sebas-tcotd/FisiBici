129d4f1a96998aea61d34361d0c707b0abb284bc
Sebastian Vargas <vargaspizango1@gmail.com>
2020-08-30 12:04:04 -0500

    Update of files

--- js/main.js
+++ js/main.js
@@ -61,21 +61,48 @@ async function deleteData(url = '') {
 
 let keys, bicis;
 const dataCargada = getData("http://localhost:5000/bicycles")
-.then(data => {
+  .then(data => {
     keys = Object.values(data.bicycles);
     console.log(keys);
     listadoBicis(data.bicycles);
-});
+  });
 
-function listadoBicis(bicis){
-    bicis.map((bici, i) => {
-        let nombre = document.createElement("p");
-        nombre.innerHTML = `Nombre de bicicleta ${i + 1}: ${bici.name}`;
-        let precio = document.createElement("p");
-        precio.innerHTML = `Precio de la bicicleta: ${bici.price}`;
-        let caja = document.querySelector("p.texto-header");
-        caja.append(nombre);
-        caja.append(precio);
-    })
+function listadoBicis(bicis) {
+  bicis.map((bici, i) => {
+    let nombre = document.createElement("p");
+    nombre.innerHTML = `Nombre de bicicleta ${i + 1}: ${bici.name}`;
+    let precio = document.createElement("p");
+    precio.innerHTML = `Precio de la bicicleta: ${bici.price}`;
+    let color = document.createElement("p");
+    color.innerHTML = `Color de la bici: ${bici.id}`
+    let caja = document.querySelector("p.texto-header");
+
+    caja.append(nombre);
+    caja.append(precio);
+    caja.appendChild(color);
+  })
 }
+
+
+// let bici1 = {
+//     "_id": "aasdasda",
+//     "name": "Denzel 122",
+//     "price": "45",
+//     "colors": ["Amarillo", "Azul"]
+// }
+
+// fetch("http://localhost:5000/bicycles", {
+//     method: 'POST',
+//     mode: 'cors',
+//     cache: 'no-cache',
+//     credentials: 'same-origin',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     redirect: 'follow',
+//     referrerPolicy: 'no-referrer',
+//     body: JSON.stringify(bici1)
+// })
+// .then(res => res.json())
+// .then(data => console.log(data))
 
