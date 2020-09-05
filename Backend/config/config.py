fe74f1c4aa496d62f4380d7473c8ea2b2813ceb7
unknown <mau9977@gmail.com>
2020-09-04 22:48:40 -0500

    Cambios implementados

--- Backend/config/config.py
+++ Backend/config/config.py
@@ -1,5 +1,3 @@
-from flask import request
-from flask_cors import CORS, cross_origin
 '''
 config.py: modulo donde se configura la aplicación
 '''
@@ -7,10 +5,6 @@ config.py: modulo donde se configura la aplicación
 
 def config_app(app):
     app.secret_key = 'clavesecreta'
-
-    # app.config['CORS_HEADERS'] = 'Content-Type'
-    # app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
-    # app.config['CORS_HEADERS'] = 'Access-Control-Allow-Credentials'
 
     app.config.update(
         SERVER_NAME='127.0.0.1:5000',
