062ad5ae4a246a23cf0fef3318f722be17462da7
unknown <mau9977@gmail.com>
2020-09-07 21:21:22 -0500

    Prueba deploy 19

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,5 @@
 '''Archivo que ejecuta la aplicacion'''
+import os
 from flask import Flask, render_template
 from flask_cors import CORS
 from mongoengine import connect
@@ -7,14 +8,15 @@ from Backend.config.config import config_app
 from Backend.routes.bicycles import create_routes_bicycles
 from Backend.routes.login import create_routes_login
 
-app = Flask(__name__)
+template_dir = os.path.abspath('../')
+app = Flask(__name__, template_folder=template_dir)
 DB_URI = "mongodb+srv://Mauricio:1234@fisibici"
 DB_URI += ".cpmx7.mongodb.net/SistemaBicicletas?retryWrites=true&w=majority"
 connect(host=DB_URI)
 
 @app.route('/')
 def index():
-    return render_template('../index.html')
+    return render_template('index.html')
 
 CORS(app=app, supports_credentials=True)
 
