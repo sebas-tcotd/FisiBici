47beea942593f92ea06c22487164acea5d27ebac
unknown <mau9977@gmail.com>
2020-09-07 19:34:23 -0500

    Prueba deploy 4

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,5 @@
 '''Archivo que ejecuta la aplicacion'''
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 from mongoengine import connect
 from config.config import config_app
@@ -10,6 +10,10 @@ app = Flask(__name__)
 DB_URI = "mongodb+srv://Mauricio:1234@fisibici"
 DB_URI += ".cpmx7.mongodb.net/SistemaBicicletas?retryWrites=true&w=majority"
 connect(host=DB_URI)
+
+@app.route('/', methods=['POST'])
+def index():
+    return "Almenos se desplegó :v"
 
 CORS(app=app, supports_credentials=True)
 
