b02f1d5ce60034a00d30019790cbfbcb784ff6bb
unknown <mau9977@gmail.com>
2020-09-08 13:43:21 -0500

    Prueba css 10

--- Backend/app.py
+++ Backend/app.py
@@ -7,7 +7,7 @@ from Backend.config.config import config_app
 from Backend.routes.bicycles import create_routes_bicycles
 from Backend.routes.login import create_routes_login
 
-app = Flask(__name__, template_folder='../Frontend/templates')
+app = Flask(__name__, template_folder='../Frontend/templates', static_url_path='../Frontend/static')
 DB_URI = "mongodb+srv://Mauricio:1234@fisibici"
 DB_URI += ".cpmx7.mongodb.net/SistemaBicicletas?retryWrites=true&w=majority"
 connect(host=DB_URI)
