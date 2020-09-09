88b6913297184705e88bd1e1ed3481d9a7192953
unknown <mau9977@gmail.com>
2020-09-08 22:23:54 -0500

    jinja2 prueba 7

--- Frontend/static/js/main.js
+++ Frontend/static/js/main.js
@@ -29,7 +29,7 @@ user.deVisitanteToUser();
 let usuarioEsta;
 
 
-if (window.location.href == "http://fisi-bici.herokuapp.com/signup") {
+if (window.location.href == "http://fisi-bici.herokuapp.com/auth/signup") {
   /** Si el usuario está en la página de REGISTRO */
   let formulario = document.querySelector("#appointment-form");
   formulario.addEventListener("submit", event => {
@@ -67,7 +67,7 @@ if (window.location.href == "http://fisi-bici.herokuapp.com/signup") {
               });
         });
   }
-  else if (window.location.href == "http://fisi-bici.herokuapp.com/singin") {
+  else if (window.location.href == "http://fisi-bici.herokuapp.com/auth/singin") {
     /** Si el usuario está en página de INICIO DE SESIÓN */
     // debugger;
     // if(usuarioEsta == true){
