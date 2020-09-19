01c5b231e590970c72057c3e47fb563c9e3e7108
unknown <mau9977@gmail.com>
2020-09-19 12:47:25 -0500

    Cambio a desarrollo

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,14 @@
 '''Archivo que ejecuta la aplicacion'''
 from flask import Flask, render_template
 
-from config.config import config_app_production
+from config.config import config_app_development
 from routes.views import create_routes_views
 from routes.bicycles import create_routes_bicycles
 from routes.login import create_routes_login
 
 app = Flask(__name__, template_folder='../Frontend/templates', static_folder='../Frontend/static')
 
-config_app_production(app)
+config_app_development(app)
 create_routes_views(app)
 create_routes_bicycles(app)
 create_routes_login(app)
